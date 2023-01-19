cbbe6159c7e06b652f5e7f8f334b4c9f09ae744d
dlord-20 <dthomaslord@gmail.com>
2023-01-19 15:48:56 +0300

    Updated organization page that will allow me to easily create an app with the features I want

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+// const container = document.getElementById('root');
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+export default function App() {
+
+  // This always loads for some reason so we'll need to fix that
+  // if(isLoading) {
+  //   return <p>Is Loading</p>
+  // }
+
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        {/* Nav */}
+        <Routes>
+          {/* Routes here with paths */}
+          {/* example | <Route path="/coaches/:id" element={<CoachPage/>}/> */}
+          <Route path="/" element={<div><p>Hello, World!</p></div>}></Route>
+        </Routes>
+        {/* Footer */}
+      </Provider>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
